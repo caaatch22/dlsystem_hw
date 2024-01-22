f16c9a3147ab5cc4af1a520db72d33894affd187
caaatch22 <mr.liumingjie@gmail.com>
2024-01-22 16:34:54 +0800

    finish hw2

--- hw2/apps/mlp_resnet.py
+++ hw2/apps/mlp_resnet.py
@@ -1,110 +1,6 @@
-# import sys
-
-# sys.path.append("../python")
-# import needle as ndl
-# import needle.nn as nn
-# import numpy as np
-# import time
-# import os
-
-# np.random.seed(0)
-# # MY_DEVICE = ndl.backend_selection.cuda()
-
-
-# def ResidualBlock(dim, hidden_dim, norm=nn.BatchNorm1d, drop_prob=0.1):
-#     ### BEGIN YOUR SOLUTION
-#     fn = nn.Sequential(nn.Linear(in_features=dim, out_features=hidden_dim),
-#                        norm(dim=hidden_dim),
-#                        nn.ReLU(),
-#                        nn.Dropout(p=drop_prob),
-#                        nn.Linear(in_features=hidden_dim, out_features=dim),
-#                        norm(dim=dim))   
-    
-#     residual = nn.Residual(fn)
-#     block = nn.Sequential(residual,
-#                           nn.ReLU())
-#     return block
-#     ### END YOUR SOLUTION
-
-
-# def MLPResNet(
-#     dim,
-#     hidden_dim=100,
-#     num_blocks=3,
-#     num_classes=10,
-#     norm=nn.BatchNorm1d,
-#     drop_prob=0.1,
-# ):
-#     ### BEGIN YOUR SOLUTION
-#     resnet = nn.Sequential(nn.Linear(dim, hidden_dim),
-#                            nn.ReLU(),
-#                            *[ResidualBlock(dim=hidden_dim, hidden_dim=hidden_dim//2, norm=norm, drop_prob=drop_prob) for _ in range(num_blocks)],
-#                            nn.Linear(hidden_dim, num_classes))
-#     return resnet
-#     ### END YOUR SOLUTION
-
-
-# def epoch(dataloader, model, opt=None):
-#     np.random.seed(4)
-#     ### BEGIN YOUR SOLUTION
-#     np.random.seed(4)
-#     ### BEGIN YOUR SOLUTION
-#     tot_loss, tot_error = [], 0.0
-#     loss_fn = nn.SoftmaxLoss()
-#     if opt is None:    
-#         model.eval()
-#         for X, y in dataloader:
-#             logits = model(X)
-#             loss = loss_fn(logits, y)
-#             tot_error += np.sum(logits.numpy().argmax(axis=1) != y.numpy())
-#             tot_loss.append(loss.numpy())
-#     else:
-#         model.train()
-#         for X, y in dataloader:
-#             logits = model(X)
-#             loss = loss_fn(logits, y)
-#             tot_error += np.sum(logits.numpy().argmax(axis=1) != y.numpy())
-#             tot_loss.append(loss.numpy())
-#             opt.reset_grad()
-#             loss.backward()
-#             opt.step()
-#     sample_nums = len(dataloader.dataset)
-#     return tot_error / sample_nums, np.mean(tot_loss)
-#     ### END YOUR SOLUTION
-
-
-# def train_mnist(
-#     batch_size=100,
-#     epochs=10,
-#     optimizer=ndl.optim.Adam,
-#     lr=0.001,
-#     weight_decay=0.001,
-#     hidden_dim=100,
-#     data_dir="data",
-# ):
-#     np.random.seed(4)
-#     ### BEGIN YOUR SOLUTION
-#     resnet = MLPResNet(28*28, hidden_dim=hidden_dim, num_classes=10)
-#     opt = optimizer(resnet.parameters(), lr=lr, weight_decay=weight_decay)
-#     train_set = ndl.data.MNISTDataset(f"{data_dir}/train-images-idx3-ubyte.gz", 
-#                                       f"{data_dir}/train-labels-idx1-ubyte.gz")
-#     test_set = ndl.data.MNISTDataset(f"{data_dir}/t10k-images-idx3-ubyte.gz",
-#                                     f"{data_dir}/t10k-labels-idx1-ubyte.gz")
-#     train_loader = ndl.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
-#     test_loader = ndl.data.DataLoader(test_set, batch_size=batch_size)
-#     for _ in range(epochs):
-#         train_err, train_loss = epoch(train_loader, resnet, opt)
-#     test_err, test_loss = epoch(test_loader, resnet, None)
-#     return train_err, train_loss, test_err, test_loss
-#     ### END YOUR SOLUTION
-
-
-# if __name__ == "__main__":
-#     train_mnist(data_dir="../data")
-import enum
 import sys
 
-sys.path.append('../python')
+sys.path.append("../python")
 import needle as ndl
 import needle.nn as nn
 import numpy as np
@@ -116,79 +12,61 @@ np.random.seed(0)
 
 def ResidualBlock(dim, hidden_dim, norm=nn.BatchNorm1d, drop_prob=0.1):
     ### BEGIN YOUR SOLUTION
-    linear1 = nn.Linear(in_features=dim, out_features=hidden_dim)
-    norm1 = norm(dim=hidden_dim)
-    relu1 = nn.ReLU()
-    dropout = nn.Dropout(p=drop_prob)
-    linear2 = nn.Linear(in_features=hidden_dim, out_features=dim)
-    norm2 = norm(dim=dim)
-    
-    fn = nn.Sequential(linear1,
-                       norm1,
-                       relu1,
-                       dropout,
-                       linear2,
-                       norm2)
+    fn = nn.Sequential(nn.Linear(in_features=dim, out_features=hidden_dim),
+                       norm(dim=hidden_dim),
+                       nn.ReLU(),
+                       nn.Dropout(p=drop_prob),
+                       nn.Linear(in_features=hidden_dim, out_features=dim),
+                       norm(dim=dim))   
     
     residual = nn.Residual(fn)
-    relu2 = nn.ReLU()
     block = nn.Sequential(residual,
-                          relu2)
+                          nn.ReLU())
     return block
     ### END YOUR SOLUTION
 
-def MLPResNet(dim, hidden_dim=100, num_blocks=3, num_classes=10, norm=nn.BatchNorm1d, drop_prob=0.1):
+
+def MLPResNet(
+    dim,
+    hidden_dim=100,
+    num_blocks=3,
+    num_classes=10,
+    norm=nn.BatchNorm1d,
+    drop_prob=0.1,
+):
     ### BEGIN YOUR SOLUTION
-    linear1 = nn.Linear(in_features=dim, out_features=hidden_dim)
-    relu1 = nn.ReLU()
-    residuals = []
-    for i in range(num_blocks):
-      res = ResidualBlock(dim=hidden_dim, hidden_dim=hidden_dim//2, norm=norm, drop_prob=drop_prob)
-      residuals.append(res)
-    linear2 = nn.Linear(in_features=hidden_dim, out_features=num_classes)
-    block = nn.Sequential(linear1,
-                          relu1,
-                          *residuals,
-                          linear2)
-    return block
+    resnet = nn.Sequential(nn.Linear(dim, hidden_dim),
+                           nn.ReLU(),
+                           *[ResidualBlock(dim=hidden_dim, hidden_dim=hidden_dim//2, norm=norm, drop_prob=drop_prob) for _ in range(num_blocks)],
+                           nn.Linear(hidden_dim, num_classes))
+    return resnet
     ### END YOUR SOLUTION
-
 
 def epoch(dataloader, model, opt=None):
     np.random.seed(4)
     ### BEGIN YOUR SOLUTION
-    if opt:
-      model.train()
-    else:
-      model.eval()
-
+    tot_loss, tot_error = [], 0.0
     loss_fn = nn.SoftmaxLoss()
-    losses = []
-    acces = []
-    correct = 0.0
-    num_samples = 0.0
-    for batch in dataloader:
-      x, y = batch[0], batch[1]
-      x = x.reshape((x.shape[0], -1))
-      # print(x.shape)
-      # print(y.shape)
-
-      y_hat = model(x)
-      loss = loss_fn(y_hat, y)
-      losses.append(loss.cached_data)
-
-      label_hat = np.argmax(y_hat.cached_data, axis=1)
-      correct += np.sum(label_hat == y.cached_data)
-      num_samples += x.shape[0]
-
-      if opt:
-        opt.reset_grad()
-        loss.backward()
-        opt.step()
-    
-    acc = correct / num_samples
-    loss_ave = np.mean(losses)
-    return 1 - acc, loss_ave
+    if opt is None:    
+        model.eval()
+        for X, y in dataloader:
+            logits = model(X.reshape((X.shape[0], -1)))
+            loss = loss_fn(logits, y)
+            tot_error += np.sum(logits.numpy().argmax(axis=1) != y.numpy())
+            tot_loss.append(loss.numpy())
+    else:
+        model.train()
+        for X, y in dataloader:
+            X = X.reshape((X.shape[0], -1))
+            logits = model(X)
+            loss = loss_fn(logits, y)
+            tot_error += np.sum(logits.numpy().argmax(axis=1) != y.numpy())
+            tot_loss.append(loss.numpy())
+            opt.reset_grad()
+            loss.backward()
+            opt.step()
+    sample_nums = len(dataloader.dataset)
+    return tot_error / sample_nums, np.mean(tot_loss)
     ### END YOUR SOLUTION
 
 
